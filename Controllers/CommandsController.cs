89a29ece7778fd8efca6c50cf98214e3dcf68c00
Savio-Fernandes-Git <saviof219@gmail.com>
2021-08-16 13:57:13 +0300

    first commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Commander.Controllers
+{
+    [ApiController] //not mandorty to this
+    [Route("api/commands")] //can write [Route("api/[controller]")] to yield the same result
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        private readonly ILogger<CommandsController> _logger;
+
+        public CommandsController(ILogger<CommandsController> logger)
+        {
+            _logger = logger;
+        }
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands(){
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id){
+            var commandItem = _repository.GetCommandyById(id);
+            return Ok(commandItem);
+        }
+    }
+}
